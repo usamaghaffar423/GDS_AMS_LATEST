d9a01ecc2190ae273960c59d86c4741e57cb88e6
Osama Ghaffar <ossamaghaffar888@gmail.com>
2025-02-07 13:26:12 +0500

    Updated Dashboard.js

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -17,7 +17,7 @@ const Dashboard = () => {
   useEffect(() => {
     const atteendanceList = async () => {
       try {
-        const response = await axios.get('http://147.93.119.175:5000/attendance/summary');
+        const response = await axios.get('http://172.16.0.3:5000/attendance/summary');
         const attendance = response.data.loggedInEmployees;
 
         console.log("Response data: ", response.data)
